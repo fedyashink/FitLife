41e851fa2bfb812624c0c83e27e28173cab312fa
Konstantin Fedyashin <fedyashink@gmail.com>
2026-09-24 18:20:16 +0300

    v2

--- fit_life.py
+++ fit_life.py
@@ -1,5 +1,6 @@
-print('=' * 20, 'FitLife Console Bot', '=' * 20)
-print('~' * 20, 'Добро пожаловать!!!', '~' * 20)
+
+print('=' * 15, 'FitLife Console Bot', '=' * 15)
+print('~' * 15, 'Добро пожаловать!!!', '~' * 15)
 
 user_name = input('Как вас зовут?: ').capitalize()
 user_age = int(input('Сколько вам лет?: '))
@@ -10,7 +11,8 @@ bmi = user_weight / (user_height ** 2)
 water_ml = user_weight * 30
 water_l = water_ml / 1000
 
-print('-' * 27, 'Итоги', '-' * 27)
+print('-' * 22, 'Итоги', '-' * 22)
 print(f'Вас зовут {user_name}, вам {user_age} лет!')
-print(f'Ваш ИМТ: {round(bmi, 1)}, а рекомендуемая норма воды: {round(water_l, 3)} литров.')
-print("Расчет окончен. Будьте здоровы!")
+print(f'Ваш ИМТ: {round(bmi, 1)}.')
+print(f'Рекомендуемая норма воды: {round(water_l, 3)} литров.')
+print('Расчет окончен. Будьте здоровы!')
